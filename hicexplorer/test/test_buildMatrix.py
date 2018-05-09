acee3ef0cc357d1d76773e3209347027eb192b14
alpha <runget@informatik.uni-freiburg.de>
2018-05-09 12:29:04 +0200

    feat(test): workaround for failing of test_build_matrix

--- hicexplorer/test/test_buildMatrix.py
+++ hicexplorer/test/test_buildMatrix.py
@@ -41,6 +41,7 @@ def test_build_matrix():
     print(set(os.listdir(ROOT + "QC/")))
     assert are_files_equal(ROOT + "QC/QC.log", qc_folder + "/QC.log")
     assert set(os.listdir(ROOT + "QC/")) == set(os.listdir(qc_folder))
+
     # accept delta of 60 kb, file size is around 4.5 MB
     assert abs(os.path.getsize(ROOT + "small_test_matrix_result.bam") - os.path.getsize("/tmp/test.bam")) < 64000
 
